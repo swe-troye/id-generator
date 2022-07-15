b6b9b53faca998950927ebe7e2a5ab5b32ad7d87
Troye <troyej112@gmail.com>
2022-07-15 16:58:17 +0800

    first commit

---
+++ src/main/java/com/swetroye/idgenerator/IdGeneratorApplication.java
@@ -0,0 +1,12 @@
+package com.swetroye.idgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdGeneratorApplication.class, args);
+	}
+}
