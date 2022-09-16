a0ff78b044d505976e6c4eb8933b15f017d7b8eb
Troye <swe.troye@gmail.com>
2022-09-16 17:36:40 +0800

    build fat jar to include redis

--- src/main/java/com/swetroye/idgenerator/config/IdGeneratorConfig.java
+++ src/main/java/com/swetroye/idgenerator/config/IdGeneratorConfig.java
@@ -1,6 +1,5 @@
 package com.swetroye.idgenerator.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
